4f721d94cb2aed1cb7732365148836953794aa3c
armjim14 <ajimenez1497@gmail.com>
2019-09-02 22:47:19 -0700

    changed cheerio file

--- routing/cheerio.js
+++ routing/cheerio.js
@@ -8,12 +8,9 @@ module.exports = (app, db) => {
 
             var $ = cheerio.load(info.data)
 
-            var send = [];
-            var send2 = [];
-            var count = 0;
-
             $("a.SQnoC3ObvgnGjWt90zD9Z").each((i, elem) => {
                 var title = $(elem).text();
+                var href = "http://reddit.com"+$(elem).attr("href");
                 let run = true;
                 db.articles.find({}, (err, data) => {
                     for( let v in data ){
@@ -22,57 +19,14 @@ module.exports = (app, db) => {
                         }
                     }
                     if ( run ){
-                        send.push(title)
-                    }
-                })
-            })
-
-            $("a.styled-outbound-link").each((i, elem) => {
-                var href = $(elem).attr("href");
-                let run = true;
-                db.articles.find({}, (err, data) => {
-                    for( let v in data ){
-                        if ( href == data[v].href ){
-                            run = false;
-                        }
-                    }
-                    if ( run ){
-                        send2.push(href);
-                    }
-                    if ( i == $("a.SQnoC3ObvgnGjWt90zD9Z").length - 1){
-                        if (count == 0){
-                            count++;
-                            nextStep();
-                        }
-                    }
-                })
-            })
-
-            function nextStep(){
-                
-                var fin = [];
-    
-                for( let i = 0; i < send.length; i++){
-                    fin.push({title: send[i], href: send2[i]})
-                }
-    
-                if( fin.length != 0 ){
-                    for (let k in fin){
-                        db.articles.create({title: fin[k].title, href: fin[k].href, comments: []})
+                        db.articles.create({title, href, comments: []})
                         .then((data) => {
                             return res.json(data)
                         })
                     }
-                } else {
-                    console.log("nothing to add")
-                }
-
-            }
-
+                })
+            })
 
         })
-        .catch((err) => {
-            console.log(err)
-        });
     })
 }
